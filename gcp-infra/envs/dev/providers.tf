1096288c990216c55fca7b3e26b06f467958557d
ali-hyder <alihyder-shaikh@outlook.com>
2026-04-04 15:40:56 +0000

    added argocd files

--- gcp-infra/envs/dev/providers.tf
+++ gcp-infra/envs/dev/providers.tf
@@ -2,3 +2,19 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
+
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = "https://${module.gke.endpoint}"
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+  token                  = data.google_client_config.default.access_token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = "https://${module.gke.endpoint}"
+    cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+    token                  = data.google_client_config.default.access_token
+  }
+}
--- gcp-infra/envs/dev/versions.tf
+++ gcp-infra/envs/dev/versions.tf
@@ -6,5 +6,15 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 6.0"
     }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.25"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12"
+    }
   }
 }
